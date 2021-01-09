c72e554cca7f5068c3f0ae4a19b9152c8a26cd6e
diegotafur <diegotafurobregon@outlook.com>
2021-01-09 16:53:11 -0500

    Se agrego carpetas css,js,asset

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+//funciones anonimas autoejecutables
+// menu
+((d) =>{
+    const $btnMenu = d.querySelector(".menu-btn"),
+    $menu = d.querySelector(".menu")
+
+    $btnMenu.addEventListener("click",(e)=>{
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $menu.classList.toggle("is-active");
+
+});
+
+//delegacion de eventos
+d.addEventListener("click",e=>{
+    if(!e.target.matches(".menu a")) return false;
+
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $menu.classList.remove("is-active");
+})
+})(document);
+
+
+
